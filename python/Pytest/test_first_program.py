3028d622755658db72d9f702d1a0e2bd65bdf10d
Narendra Nishad <narennishad@gmail.com>
2024-03-31 23:19:58 +0530

    pytest practice
    pytest

---
+++ python/Pytest/test_first_program.py
@@ -0,0 +1,16 @@
+def test_login():
+    print("Login Successfully")
+
+def test_logoff():
+    print("Logoff Successfully")
+
+
+def test_check_calculation():
+    a = 10
+    if a<1:
+        raise Exception("Values do not match")
+        assert("Values do not match")
+
+
+
+
